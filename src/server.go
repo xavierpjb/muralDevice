c11d4787cdab55728a79c58d6a556ad1c9467398
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2021-02-16 10:41:46 -0500

    Update server module

--- src/server.go
+++ src/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	artifact "artifact"
@@ -19,29 +18,5 @@ func main() {
 }
 
 func getterPoster(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("get poster called")
-	if r.URL.Path != "/" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	switch r.Method {
-	case "GET":
-		fmt.Println("get poster called in get method")
-
-		http.ServeFile(w, r, "artifact.html")
-	case "POST":
-		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
-		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
-		name := r.FormValue("name")
-		address := r.FormValue("address")
-		fmt.Fprintf(w, "Name = %s\n", name)
-		fmt.Fprintf(w, "Address = %s\n", address)
-	default:
-		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
-	}
+	http.ServeFile(w, r, "artifact.html")
 }
