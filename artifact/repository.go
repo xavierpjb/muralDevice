7b7719a5d67251853e8cf8332404f87ddf517558
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2021-02-24 12:54:50 -0500

    rename mongo url to container

--- artifact/repository.go
+++ artifact/repository.go
@@ -58,7 +58,7 @@ func (a ArtifactRepositoryHandler) RetrieveList() {
 }
 
 func Dbdriver() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://mongo:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
