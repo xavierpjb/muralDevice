75889b1d224515cde6bb331280499a04e2ba5095
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2021-03-03 09:23:27 -0500

    Update tests to include username

--- artifact/artifact.go
+++ artifact/artifact.go
@@ -16,18 +16,6 @@ import (
 	"github.com/spf13/afero"
 )
 
-// ArtifactModel represent the model to imitate an artifact (file/mp4 etc)
-type ArtifactModel struct {
-	Username string
-	File     string
-	Type     string
-}
-
-// IsPersistable checks that the properties need to persist an artifact are all present
-func (a ArtifactModel) IsPersistable() bool {
-	return a.File != "" && a.Type != "" && a.Username != ""
-}
-
 // Artifact base constructor for construtor to fs
 type Artifact struct {
 	fileSystem                afero.Fs
