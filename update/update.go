00f4d72e99afe7908eeb9d605da7af24c6880052
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2021-03-06 17:25:34 -0500

    clean up update code

--- update/update.go
+++ update/update.go
@@ -2,22 +2,24 @@ package update
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 
 	"github.com/spf13/afero"
 )
 
+// Updater struct to handle files to be used for updating containers
 type Updater struct {
 	fileSystem afero.Fs
 }
 
+// New instantiates updater
 func New(fileSystem afero.Fs) Updater {
 	a := Updater{fileSystem}
 	return a
 }
 
+// HandleUpdate is used for the endpoint
 func (u Updater) HandleUpdate(w http.ResponseWriter, r *http.Request) {
 	var (
 		status int
@@ -25,23 +27,29 @@ func (u Updater) HandleUpdate(w http.ResponseWriter, r *http.Request) {
 	)
 	defer func() {
 		if nil != err {
-			// http.Error(r, err.Error(), status)
 			fmt.Println(status)
-
 		}
 	}()
 	switch r.Method {
 	case "POST":
-		fmt.Println("post called")
+		fmt.Println("Update endpoint called, Parsing file")
 		r.ParseMultipartForm(32 << 20)
 		fmt.Println(r.MultipartForm.File)
 		cont := r.MultipartForm.Value["filer"]
-		_ = ioutil.WriteFile("containerFiles/mural_dev.tar.gz", []byte(cont[0]), 0666)
+
+		fmt.Println("Writing tar file to fs")
+		err = afero.WriteFile(u.fileSystem, "containerFiles/mural_dev.tar.gz", []byte(cont[0]), 0666)
+		if err != nil {
+			fmt.Println("There was an issue saving to fs")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("Writing update instruction so system pipe")
 		stdout, _ := u.fileSystem.OpenFile("containerFiles/update", os.O_WRONLY, 0600)
 		stdout.Write([]byte("cd /home/ubuntu/ && touch thisIsFromGo && docker-compose down && cd containerFiles && (docker load < mural_dev.tar.gz) && cd .. && docker-compose up -d"))
+		fmt.Println("Done writing, containers will start shutting down")
 		stdout.Close()
-		// stdout.Write([]byte("cd /home/ubuntu/ && docker-compose down && cd containerFiles && (docker load < mural_dev.tar.gz) && cd .. &&  docker-compoes up -d"))
-
 	}
 
 }
